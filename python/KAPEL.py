1ee048392bc8580129dc52f85be917ab6a2b7569
Matthew Westphall <westphall@wisc.edu>
2026-03-09 12:34:51 -0500

    Add debug logging for records skipped due to missing pod UID

--- python/KAPEL.py
+++ python/KAPEL.py
@@ -64,7 +64,7 @@ class QueryLogic:
             f'(max_over_time(kube_pod_container_resource_requests{{resource="cpu", node != "", namespace="{namespace}"}}[{queryRange}]) '
             f'or on(pod, uid, namespace) max_over_time(kuantifier_pod_cpu_requests{{namespace="{namespace}"}}[{queryRange}]))'
         )
-        
+
         #Calculates each pod’s CPU time by multiplying how long it ran by its CPU cores, using exporter data if KSM metrics are missing.
         self.cputime = (
         f'(max by (pod, uid) ('
@@ -210,20 +210,25 @@ def get_gap_time_periods(start, end):
 
 
 def filter_records_by_uid(prom_result: list[dict[str, dict[str, Any]]]):
-    # For a given set of prometheus metrics, gracefully filter out any metrics that do 
-    # not have a pod UID field set. We expect that all kube-state-metrics metrics, as 
+    # For a given set of prometheus metrics, gracefully filter out any metrics that do
+    # not have a pod UID field set. We expect that all kube-state-metrics metrics, as
     # well as custom metrics exported by runtime_exporter.py, should always have this
     # field set.
-    return (item for item in prom_result if item['metric'].get('uid'))
+    filtered_items = [item for item in prom_result if item['metric'].get('uid')]
+    skipped_count = len(prom_result) - len(filtered_items)
+    return filtered_items, skipped_count
 
 # Take a list of dicts from the prom query and construct a random-accessible dict (casting from string to float while we're at it) via generator.
 # (actually a list of tuples, so use dict() on the output) that can be referenced by the (pod, uid) label as a key.
 def group_results_by_pod_uid(prom_result: list[dict[str, Any]]):
-    for item in filter_records_by_uid(prom_result):
+    filtered_items, skipped_count = filter_records_by_uid(prom_result)
+    uid_groups = []
+    for item in filtered_items:
         pod_key = item['metric']['pod']
         uid_key = item['metric']['uid']
         # this produces each of the (key, value) tuples in the list
-        yield (pod_key, uid_key), float(item['value'][1])
+        uid_groups.append(((pod_key, uid_key), float(item['value'][1])))
+    return uid_groups, skipped_count
 
 
 def record_summarized_period(config, period_start, year, month, results):
@@ -302,10 +307,10 @@ def record_individual_period(config, results: dict[str, dict[tuple[str, str], fl
     per_pod_records = {}
     for data_type, records in results.items():
         for (pod, uid), val in records.items():
-            if not (pod, uid) in per_pod_records:
+            if (pod, uid) not in per_pod_records:
                 per_pod_records[(pod, uid)] = {}
             per_pod_records[(pod, uid)][data_type] = val
-    
+
     dirq = QueueSimple(str(config.output_path))
     skipped_records = 0
     for (pod_name, uid), records in per_pod_records.items():
@@ -321,7 +326,7 @@ def record_individual_period(config, results: dict[str, dict[tuple[str, str], fl
             continue
 
         individual_output = individual_message(
-            config, 
+            config,
             pod_name,
             uid,
             records.get('memory', 0),
@@ -333,7 +338,7 @@ def record_individual_period(config, results: dict[str, dict[tuple[str, str], fl
         record_file = dirq.add(individual_output)
         print(f'Writing individual record to {config.output_path}/{record_file}:')
         print('--------------------------------\n' + individual_output + '--------------------------------')
-    
+
     if skipped_records > 0:
         print(f"WARNING: Skipped {skipped_records} records due to missing processor count. "
                "Please set pod resource requests or specify the PROCESSORS config var.")
@@ -375,6 +380,7 @@ def process_period(config, period):
     prom_connect_params = {'time': period['instant'].isoformat(), 'timeout': config.query_timeout}
 
     results = {}
+    skipped_count = 0
     # iterate over each query (cputime, starttime, endtime, cores) producing raw_results['cputime'] etc.
     for query_name, query_string in vars(queries).items():
         # Each of these raw_results is a list of dicts. Each dict in the list represents an individual data point, and contains:
@@ -384,7 +390,9 @@ def process_period(config, period):
         t1 = timer()
         raw_result = prom.custom_query(query=query_string, params=prom_connect_params)
         t2 = timer()
-        results[query_name] = dict(group_results_by_pod_uid(raw_result))
+        grouped_records, skipped_records = group_results_by_pod_uid(raw_result)
+        results[query_name] = dict(grouped_records)
+        skipped_count += skipped_records
         t3 = timer()
         print(f'Query finished in {t2 - t1} s, processed in {t3 - t2} s. Got {len(results[query_name])} items from {len(raw_result)} results. Peak RAM usage: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss}K.')
 
@@ -393,6 +401,9 @@ def process_period(config, period):
     else:
         record_individual_period(config, results)
 
+    if skipped_count > 0:
+        print(f"WARNING: Skipped {skipped_count} records due to missing pod UID field.")
+
 def main(envFile):
     print(f'Starting KAPEL processor: {__file__} with envFile {envFile} at {datetime.datetime.now(tz=datetime.timezone.utc).isoformat()}')
     cfg = KAPELConfig(envFile)
@@ -400,7 +411,7 @@ def main(envFile):
     # look for manually-defined records, from the manual configmap
     manual_path = '/srv/manual'
     found_records = [ f for f in listdir(manual_path) if isfile(join(manual_path, f))]
-    
+
     if found_records:
       print('Manually-defined records detected in ' + manual_path)
       # Because dirq.add_path uses hard links, can't cross filesystems, have to copy files
